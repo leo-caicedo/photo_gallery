6c41415855efc9c59684e9c87b236a0f65f73686
Leonardo Caicedo <devleonardocaicedo@gmail.com>
2021-05-27 15:53:07 -0500

    logout implementation

--- users/urls.py
+++ users/urls.py
@@ -4,9 +4,11 @@ from django.urls import path
 
 # Views
 from users.views import main, Login
+from django.contrib.auth.views import LogoutView
 
 app_name = 'users'
 urlpatterns = [
     path('', main),
     path('login', Login.as_view(), name='login'),
+    path('logout', LogoutView.as_view(), name='logout'),
 ]
